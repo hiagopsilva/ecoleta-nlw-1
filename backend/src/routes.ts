13f15fc0c0a5d0d703ddd00f4c39453b0b65a148
hiagopsilva <hiagohps360@gmail.com>
2020-06-02 20:05:01 -0300

    add routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World'})
+});
+
+export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,40 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json())
-
-const users = [
-  'Hiago',
-  'Diego',
-  'Robson'
-]
-
-app.get('/users', (req, res) => {
-  const search = String(req.query.search);
-
-  const filteredSearch = search ? users.filter(user => user.includes(search)) : users;
-
-  return res.json(filteredSearch);
-});
-
-app.get('/users/:id', (req, res) => {
-  const id = Number(req.params.id);
-
-  const user = users[id]; 
-
-  return res.json(user);
-})
-
-app.post('/users', (req, res) => {
-  const data = req.body;
-  
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return res.json(user);
-})
+app.use(routes);
 
 app.listen(3333);
