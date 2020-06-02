449da307d3e0186260e0a5aeaecd558c517eaf39
Hiago Prates <hiagohps360@gmail.com>
2020-06-02 17:35:11 -0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,20 +2,36 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Listagem de usuários');
+app.use(express.json())
 
-  res.json([
-    'Hiago',
-    'Diego',
-    'Robson'
-  ]);
+const users = [
+  'Hiago',
+  'Diego',
+  'Robson'
+]
+
+app.get('/users', (req, res) => {
+  const search = String(req.query.search);
+
+  const filteredSearch = search ? users.filter(user => user.includes(search)) : users;
+
+  return res.json(filteredSearch);
 });
 
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users[id]; 
+
+  return res.json(user);
+})
+
 app.post('/users', (req, res) => {
+  const data = req.body;
+  
   const user = {
-    name: 'Hiago',
-    email: 'hiago@gmail.com'
+    name: data.nome,
+    email: data.email,
   };
 
   return res.json(user);
