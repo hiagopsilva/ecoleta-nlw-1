f7561d1186825b12b5f823caccf6ac67ed5a5af8
hiagopsilva <hiagohps360@gmail.com>
2020-06-05 23:38:30 -0300

    registration of the location in the api

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -29,6 +29,8 @@ const CreatePoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
   
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+  
   const [selectedUf, setSelectedUf] = useState('0')
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
@@ -61,6 +63,14 @@ const CreatePoint = () => {
         setCities(cityNames);
     })
   }, [selectedUf])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      
+      setInitialPosition([latitude, longitude])
+    })
+  }, [])
 
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
@@ -137,7 +147,7 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-23.6260084, -46.7768741]} zoom={14}  onclick={handleMapClick} >
+          <Map center={initialPosition} zoom={14}  onclick={handleMapClick} >
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
