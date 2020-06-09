434d997eeb8189650a2c18b7b1571872046cd764
hiagopsilva <hiagohps360@gmail.com>
2020-06-08 23:45:38 -0300

    finish Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,3 +1,48 @@
+import React from 'react';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+const Detail = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack()
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack} >
+            <Icon name="arrow-left" size={20} color="#34cb79" />  
+          </TouchableOpacity>
+
+          <Image style={styles.pointImage} source={{ uri: 'https://bemresolvida.com.br/wp-content/uploads/2019/09/Legendas-para-fotos-com-paisagens-02-740x522.jpg'}} />
+          
+          <Text style={styles.pointName}>Mercado do João</Text>
+          <Text style={styles.pointItems}>Lampadas, oleo de cozinha</Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+          </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View> 
+    </SafeAreaView>
+  ) 
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -71,3 +116,5 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto_500Medium',
   },
 });
+
+export default Detail;
